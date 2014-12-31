7c5beda7bd9f0eb076227aaaf218408669a7c5e6
iTech75 <tekinaytekin@hotmail.com>
2014-12-31 12:16:40 +0200

    Revert "UI deneme"
    This reverts commit 27bf794428fca081257eb29b4fd8ed2f401c76e5.

--- Server/BGServer.py
+++ Server/BGServer.py
@@ -3,6 +3,7 @@ import Player
 __author__ = 'Tekin.Aytekin'
 import socket
 import threading
+import Queue
 
 
 class BGServer(threading.Thread):
@@ -10,7 +11,7 @@ class BGServer(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.__serverSocket = socket.socket()
-        self.__playerDictionary = {}
+        self.__playerQueue = Queue.Queue()
 
     def run(self):
         self.__serverSocket.bind(("localhost", 18475))
@@ -18,13 +19,13 @@ class BGServer(threading.Thread):
         while 1:
             client_socket, client_address = self.__serverSocket.accept()
             new_player = Player.Player(self, client_socket, client_address)
-            self.add_player_to_dictionary(client_address, new_player)
+            self.add_player_to_queue(new_player)
             new_player.start()
             if __debug__:
                 print "New player arrives to the server"
 
-    def add_player_to_dictionary(self, client_address, new_player):
-        self.__playerDictionary[client_address] = new_player
+    def add_player_to_queue(self, new_player):
+        self.__playerQueue.put(new_player)
         new_player.start()
 
 
